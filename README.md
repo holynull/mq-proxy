0861ea273f2d615fadd59cfc21d48cbe73a30ec2
holynull <holynull@126.com>
2022-09-20 15:08:55 +0800

    introductions

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # mq-proxy
+
+***前提条件：***需要一个rabbitmq服务，或者使用脚本启动一个rabbitmq的docker容器，`./start-docker-rabbit.sh`
+
+** 运行说明
+
+mq_proxy能够接收一个名为target的参数，当target等于client时，表示启动运行在enclave外部宿主机上的程序；当target等于server时，表示启动运行在enclave内部的程序。
+
+当target为client时，还需要另外两个参数cid和port；本别表示enclave或者容器的cid和vsock服务的端口号。
+
+```
+# 在宿主机上
+mq_proxy --target=client --cid=3 --port=3000
+
+# 在enclave内
+mq_proxy --target=server
+
+```
+
+启动顺序：先启动enclave或者容器内部的server程序，再启动宿主机上的client。
+
+如果在mac上调试开发，可以使用`go mq_proxy.go --target=client --cid=3 --port=3000`启动宿主机上的程序。
+
+而创建docker的镜像，请执行如下脚本
+
+```
+./build.sh
+./clear_images.sh
+./build-dockcer-image.sh
+```
+
+然后启动docker镜像，模拟enclave环境，请执行如下脚本
+
+```
+./start-docker-proxy-server.sh
+```
+
--- mq_proxy.go
+++ mq_proxy.go
@@ -18,29 +18,34 @@ func main() {
 	flag.Parse()
 	switch target {
 	case "client":
+		// 初始化一个client
 		client := parentproxy.NewProxyClient()
+		// 启动client的vsock连接；vsock就绪后，启动mq的client连接；
 		go client.StartVsockClient(uint32(cid), uint32(port))
 	Loop:
 		for {
-			status := <-client.StatusChan
+			status := <-client.StatusChan // 接受vsock连接状态
 			switch status {
-			case my_vsock.CONNECTED_OK:
+			case my_vsock.CONNECTED_OK: // vsock连接成功
+				// 向enclave或者容器内部发送一则消息
 				client.SendMessageToInside([]byte("Message request some inside service."))
-			case my_vsock.VSOCK_EOF:
+			case my_vsock.VSOCK_EOF: // vsock服务器端端开连接
 				break Loop
 			}
 		}
 	case "server":
+		// 初始化服务器端；即enclave或者容器端
 		server := parentproxy.NewProxyServer()
+		// 启动vsock服务端
 		go server.StartServer()
 		for {
 			select {
-			case pStatus := <-server.ProxyStatusChan:
-				if pStatus == parentproxy.PROXY_READY {
-					server.BroadcastMessage([]byte("lllladfadfadfadfa"))
-					server.SendMessageToNode([]byte("Send a message to node 2"), "party_node_2")
+			case pStatus := <-server.ProxyStatusChan: // 宿主端代理的服务状态
+				if pStatus == parentproxy.PROXY_READY { // 宿主端代理服务状态准备就绪
+					server.BroadcastMessage([]byte("lllladfadfadfadfa"))                         // 试着广播一个消息
+					server.SendMessageToNode([]byte("Send a message to node 2"), "party_node_2") // 向node id为party_node_2的节点发送一则消息
 				}
-			case msgFromOutside := <-server.MessageFromOutSide:
+			case msgFromOutside := <-server.MessageFromOutSide: // 接收从宿主端发送进来的其他信息
 				log.Printf("Get message from outside: %s", msgFromOutside)
 			}
 		}
